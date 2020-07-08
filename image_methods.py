2d68e8a79619dfe591457dd50373d0a3219a5cfe
Drew Davie <68015795+drewdavie@users.noreply.github.com>
2020-07-08 14:44:57 +0100

    Initial work
    Initial copies of the image viewer and the accompanying module.

---
+++ image_methods.py
@@ -0,0 +1,153 @@
+import numpy as np
+import scipy.interpolate as interpolate
+import matplotlib.pyplot as plt
+    
+#https://stackoverflow.com/questions/44865023/circular-masking-an-image-in-python-using-numpy-arrays masking explanation 
+
+def PIU(pixels):
+
+    #create a mask covering a central portion of the circular image, assuming it is centrally positioned
+    nrows, ncols = pixels.shape
+    centre = [nrows/2,ncols/2]
+    #5.5 is jut an emperical cut-off for the radius of the mask 
+    radius = min(nrows, ncols)/5.5 
+    X, Y = np.ogrid[:nrows,:ncols]
+    dist_from_centre = np.sqrt((X-centre[0])**2 + (Y-centre[1])**2)
+    mask = dist_from_centre <= radius
+
+    masked_pixels = pixels.copy()
+    #by setting everything outside the main mask to 0, can assume that our min inside the ROI is > 0 so can be pinned down
+    masked_pixels[~mask]=0
+    
+    #make small mask max/min regions from which we get the mean to calculate the PIU
+    maxindices = np.where(masked_pixels  == masked_pixels.max())
+    dist_from_max = np.sqrt((X-maxindices[0][0])**2 + (Y-maxindices[1][0])**2)
+    maxmask = dist_from_max <=2
+
+    #everything outside of the main mask is 0, so find the minimum nonzero 0 value within the mask
+    minindices = np.where(masked_pixels  == min(masked_pixels[np.nonzero(masked_pixels)]))
+    dist_from_min = np.sqrt((X-minindices[0][0])**2 + (Y-minindices[1][0])**2)
+    minmask = dist_from_min <=2
+
+
+    #in a very uniform image the maxmean can be lower then the minmean giving a PIU of over 100
+    maxmean = pixels[maxmask].mean()
+    print(maxmean)
+    minmean = pixels[minmask].mean()
+    print(minmean)
+    PIU = 100*(1-((maxmean-minmean)/(maxmean+minmean)))
+    
+    #show the main mask used and the location of the max and min masks
+    #plt.figure(1)
+    #plt.imshow(masked_pixels)
+    max_min_pixels = pixels.copy()
+    max_min_pixels[maxmask]=5000
+    max_min_pixels[minmask]=2500
+    #plt.figure(2)
+    #plt.imshow(max_min_pixels)
+    #plt.show()
+    
+    #return the pixels with the ROIs identified and show in the window 
+    return PIU, max_min_pixels
+
+def profiles(pixels, coords):
+
+        #have to use np.copy, can't directly assign as something strange happens...
+        profile_pix = np.copy(pixels)
+        #set column with x, row range with y, y2
+        x, y, y2 = int(coords[0][0]), int(coords[0][1]), int(coords[1][1])
+
+        #set row with y3, row ranage with x3, x4
+        x3, y3, x4 = int(coords[2][0]), int(coords[2][1]), int(coords[3][0])
+                 
+        #show on graph the chosen range, width is thickness of rectangle to average over in pixels
+        #15 works for a fairly high res image, e.g. a scanner import of a film profile 
+        width = 15
+        #just setting a different shades for the profile ranges so that we plot them and yet the user see 
+        profile_pix[y:y2,x-width:x+width] = 10
+        profile_pix[y3-width:y3+width,x3:x4] = 10
+
+        #think the hor and ver labels are the wrong way around 
+        #copy the selected ranges for each axis and take the median of the rows/columns to average out any artefacts
+        hor_data = np.copy(pixels[y:y2,x-width:x+width])
+        hor_data = np.median(hor_data, axis=1)
+        ver_data = np.copy(pixels[y3-width:y3+width, x3:x4])
+        ver_data = np.median(ver_data, axis=0)
+        
+        #process the chosen profile ranges, get three different sections of the range
+        hor_prof, hor_fwhm, hor_beam, hor_flatness, hor_symmetry = process_profile(hor_data, "X")
+        ver_prof, ver_fwhm, ver_beam, ver_flatness, ver_symmetry = process_profile(ver_data, "Y")
+        
+        #could do with titles/axes to embellish stuff
+        plt.figure(1)
+        plt.plot(hor_prof)
+        plt.plot(hor_fwhm)
+        plt.plot(hor_beam)
+        plt.figure(2)
+        plt.plot(ver_prof)
+        plt.plot(ver_fwhm)
+        plt.plot(ver_beam)
+        #block allows programme to continue, otherwise it will wait until plts destroyed
+        plt.show(block=False)
+
+        #not returning these values for some reason, but it was plaotting?
+        return profile_pix, hor_flatness, hor_symmetry, ver_flatness, ver_symmetry
+
+    
+def process_profile(profile, axis):
+
+        #invert and normalise profiles - inversion required for film, could make a check to identify if this is required
+        if max(profile) == 0:
+            #Print error message, carry on calculating and give zero answer otherwise functions above break
+            print("Invalid area selected, no pixel value above 0.")
+        else:
+            profile = np.divide(profile,max(profile))
+
+        profile[:] = [1-i for i in profile]
+
+        if max(profile) == 0:
+            print("Invalid area selected, no pixel value above 0.")
+        else:
+            profile = np.divide(profile,max(profile))
+            
+
+        #interpolate profile over a smaller grid to give more granularity as data is sparse especially in penumbra
+        xaxis = np.arange(len(profile))
+        #interp acts as interpolation to feed in new grid size
+        interp = interpolate.interp1d(xaxis,profile,kind='linear')
+        #create the new grid  with 10 times as many points as the original grid, new x points spread between integers
+        x_interp = np.linspace(0,len(profile)-1,num=len(profile)*10)
+        profile = interp(x_interp)
+        
+        #find half maximunm on each side: abs difference between points on each side and half maximum, then minimise with argmin
+        first_half_max = np.abs(profile[0:int(len(profile)/2)] - max(profile)/2).argmin()
+        second_half_max = np.abs(profile[int(len(profile)/2):] - max(profile)/2).argmin() + int(len(profile)/2)
+        fwhm = second_half_max-first_half_max
+
+        #generate fwhm and beam for plotting, beam uses middle 80%, so take off 10% at each end
+        fwhm_plot = profile[first_half_max:second_half_max]
+        beam = profile[first_half_max+int(fwhm/10):second_half_max-int(fwhm/10)]
+
+        #calc flatness and symmetry according to equations
+        flatness = 100*((max(beam)-min(beam))/(max(beam)+min(beam)))
+        #print("Flatness: " + str(flatness))
+
+        #if useful beam is an odd integer remove a central data point, otherwise the symmetry calculations fail
+        #sym_array outputs all of the individual ratios of the data points from each side of the profile to compare with the manual method
+        if len(beam) % 2 == 0:
+            sym_array = beam[0:int(len(beam)/2)]/np.flip(beam[int(len(beam)/2):len(beam)])
+        else:
+            beam = np.delete(beam, int(len(beam)/2)+1)
+            sym_array = beam[0:int(len(beam)/2)]/np.flip(beam[int(len(beam)/2):len(beam)])
+        
+        #np.savetxt('P:/RP1Rotation/RP1-C-4 EvaluateBeamProfileAndDoseOutputkVMachine/Films/output' + axis + '.txt' , sym_array)
+
+        #calculate symmetry in a different way by summing over the whole area of each profile half and finding the difference
+        symmetry = 100*(np.sum(beam[0:int(len(beam)/2)]) - np.sum(beam[int(len(beam)/2):len(beam)]))/(np.sum(beam[0:int(len(beam)/2)]) + np.sum(beam[int(len(beam)/2):len(beam)]))
+        #print("Symmetry: " + str(symmetry))
+
+        #hacky, concat nans to shift into correct plotting position after the cut off from the first half maximum  
+        fwhm_plot = np.concatenate([np.full(first_half_max, np.nan),fwhm_plot])
+        beam = np.concatenate([np.full(first_half_max+int(fwhm/10),np.nan),beam])
+        
+        return profile, fwhm_plot, beam, flatness, symmetry
---
+++ imageviewer.py
@@ -0,0 +1,194 @@
+#GUI design based on the following tutorial:
+#https://pythonprogramming.net/object-oriented-programming-crash-course-tkinter/?completed=/tkinter-depth-tutorial-making-actual-program/
+
+#This programme opens up images (nominally DICOMs), can scroll through 3D images also.
+#For 2D images, can automatically calculate PIU and flatness and symmetry from profiles, although is currently only setup to work when the signal is darker than the background
+
+import tkinter as tk
+from tkinter import messagebox
+from tkinter.filedialog import askopenfilename
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+import pydicom
+import numpy as np
+import image_methods as img_methods
+import importlib
+importlib.reload(img_methods)
+from PIL import Image
+
+LARGE_FONT= ("Verdana", 12)
+
+#parentheses on a class indicate inheritance
+class ImageViewer(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+
+        tk.Tk.__init__(self, *args, **kwargs)
+        tk.Tk.wm_title(self, "DICOM Viewer")
+        
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand = True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+	#create empty dict to fill with frames
+        self.frames = {}
+        
+        for F in (StartPage, ImagePage):
+
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+        
+        button = tk.Button(self, text="ImagePage", command=lambda: controller.show_frame(ImagePage))
+        button.pack()     
+        
+class ImagePage(tk.Frame):
+   
+    def __init__(self, parent, controller):
+    
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Image Viewer", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        home_button = tk.Button(self, text="Back to Home", command=lambda: controller.show_frame(StartPage))
+        home_button.pack()
+       
+        load_button = tk.Button(self, text="Browse...", command=self.load_file)
+        load_button.pack() 
+        
+        self.f = Figure(figsize=(6,6), dpi=100)
+        self.a = self.f.add_subplot(1,1,1)
+        self.canvas = FigureCanvasTkAgg(self.f, self)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        
+        toolbar = NavigationToolbar2Tk(self.canvas, self)
+        toolbar.update()
+        self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        #these actions fail on a 3D image (and not gracefully!), could just force them to analyse the current slice
+        self.PIU_button = tk.Button(self, text="Calculate PIU", command=lambda: self.get_results(img_methods.PIU))
+        self.PIU_button.pack()
+        self.profiles_button = tk.Button(self, text="Profiles", command=self.profile_click)
+        self.profiles_button.pack()
+        
+        self.reset_button = tk.Button(self, text="Reset", command=self.plot_image)
+        self.reset_button.pack()
+                
+        # create empty array to allow the buttons to not fail if image hasn't been loaded, can probably be done in a nicer way 
+        self.pixels = []
+                                           
+    def load_file(self):
+
+        #increase range to png, tif, whatever
+        filename = askopenfilename(filetypes=(("DICOM Files", "*.dcm"), ("JPEG Files", "*.jpg"), ("All Files", "*.*")))
+        
+        if not filename:
+            return
+        
+        if filename.endswith('.dcm'):
+            ds=pydicom.read_file(filename)
+            self.pixels = ds.pixel_array
+        else:
+            #convert image to grayscale so that it can be analysed as a 2D arrayd in just 2D
+            self.pixels = Image.open(filename).convert('L')
+            self.pixels = np.asarray(self.pixels, dtype="int16")
+        
+        self.plot_image()
+ 
+    def plot_image(self):
+
+        if self.pixels.ndim > 2:
+            self.plot_slices()
+        else:
+            self.a.clear()
+            self.a.imshow(self.pixels, cmap='gray')       
+            self.canvas.draw()
+
+    def plot_slices(self):
+
+        self.slices = self.pixels.shape[0]
+        #// performs integer division rather than floating point
+        self.ind = self.slices//2
+        self.a.clear()
+        self.a.imshow(self.pixels[self.ind], cmap='jet')
+        self.a.set_xlabel('Slice %s' % self.ind)
+        self.canvas.draw()
+        self.scroll = self.f.canvas.mpl_connect('scroll_event', self.onscroll)
+
+    def onscroll(self, event):
+        
+        #if 2D image loaded after 3D image prevent the scrolling functions doing anything while leaving scrolling active
+        if self.pixels.ndim < 3:
+            return
+        
+        if (event.button == 'up') and (self.ind < (self.slices - 1)):
+            self.ind = (self.ind + 1) % self.slices
+            self.update_slice()
+        elif (event.button == 'down') and (self.ind > 0):
+            self.ind = (self.ind - 1) % self.slices
+            self.update_slice()
+
+    def update_slice(self):
+
+        self.a.clear()
+        self.a.imshow(self.pixels[self.ind], cmap='jet')
+        self.a.set_xlabel('Slice %s' % self.ind)
+        self.canvas.draw()
+        
+    def get_results(self, method):
+        #generic function for obtaining a numerical parameter such as uniformity
+
+        if len(self.pixels) > 0:
+            method_name = method.__name__
+            result, analysed_pixels = method(self.pixels)
+            messagebox.showinfo(method_name, method_name + ": " + str(result))
+            self.a.clear()
+            self.a.imshow(analysed_pixels)       
+            self.canvas.draw()
+
+    def profile_click(self):
+
+        #activate the clicking, let the onclick event record data, then profiles method processes
+        self.cid = self.f.canvas.mpl_connect('button_press_event', self.profile_coords)
+        messagebox.showinfo("Profiles", "Click Top, Bottom, Left, then Right to select profile edges.")
+        self.coords = []   
+        
+    def profile_coords(self, event):
+
+        #lets you click and records four sets of co-ordinates
+        ix, iy = event.xdata, event.ydata
+        self.coords.append((ix, iy))
+
+        if len(self.coords) > 3:
+            #disconnect and calculate profiles
+            self.f.canvas.mpl_disconnect(self.cid)
+            if self.pixels.ndim < 3:
+                analysed_pixels, horf, hors, verf, vers = img_methods.profiles(self.pixels, self.coords)
+            else:
+                analysed_pixels, horf, hors, verf, vers = img_methods.profiles(self.pixels[self.ind], self.coords)
+            
+            self.a.clear()
+            self.a.imshow(analysed_pixels)       
+            self.canvas.draw()
+            print(horf, hors, verf, vers)
+
+app = ImageViewer()
+app.mainloop()
